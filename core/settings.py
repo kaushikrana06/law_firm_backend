cc0bbfc48e57712f3eb662ee3f7ccc737a167fff
kaushikrana06 <kaushikrana.0603@gmail.com>
2025-10-04 00:29:29 +0530

    fix

--- core/settings.py
+++ core/settings.py
@@ -144,17 +144,16 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "verbose": {"format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}", "style": "{"},
+        "verbose": {"format": "{levelname} {asctime} {module} {message}", "style": "{"},
         "simple": {"format": "{levelname} {message}", "style": "{"},
     },
     "handlers": {
-        "file": {"level": "INFO", "class": "logging.FileHandler", "filename": LOG_DIR / "django.log", "formatter": "verbose"},
-        "console": {"level": "DEBUG", "class": "logging.StreamHandler", "formatter": "simple"},
+        "console": {"class": "logging.StreamHandler", "formatter": "simple"},
     },
-    "root": {"handlers": ["console", "file"], "level": "INFO"},
+    "root": {"handlers": ["console"], "level": "INFO"},
     "loggers": {
         "django.security": {"handlers": ["console"], "level": "INFO", "propagate": False},
-        "authentication": {"handlers": ["console", "file"], "level": "INFO", "propagate": False},
+        "authentication": {"handlers": ["console"], "level": "INFO", "propagate": False},
     },
 }
 
