108b013c5f02dbcb7ec051a27d17d0973be670b9
kaushikrana06 <kaushikrana.0603@gmail.com>
2025-10-03 10:00:42 +0530

    lifetime span changed

--- core/settings.py
+++ core/settings.py
@@ -145,8 +145,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
