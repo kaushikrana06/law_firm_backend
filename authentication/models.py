3718e54aa21902aa8d38bec02250dc9beed9360f
kaushikrana06 <kaushikrana.0603@gmail.com>
2025-10-04 15:19:17 +0530

    super user

--- authentication/models.py
+++ authentication/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.template import TemplateDoesNotExist
 from django.utils import timezone
@@ -13,6 +13,32 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+class CustomUserManager(UserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        # ensure we have a username even if not provided
+        username = extra_fields.pop("username", None) or email.split("@")[0]
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     is_email_verified = models.BooleanField(default=False)
@@ -24,6 +50,7 @@ class CustomUser(AbstractUser):
     deleted_at = models.DateTimeField(blank=True, null=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+    objects = CustomUserManager()
     class Meta:
         db_table = 'users'
         verbose_name = 'User'
