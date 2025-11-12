a8adb8352b7bdea64a6cae73f47113d0bb8dfb76
kaushikrana06 <kaushikrana.0603@gmail.com>
2025-11-12 16:20:35 +0530

    signal chnged

--- cases/signals.py
+++ cases/signals.py
@@ -1,11 +1,11 @@
 # cases/signals.py
 from typing import List
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.utils import timezone
 
-from .models import Case
+from .models import Case, CaseNote
 from notifications.services import notify_client_case_updated
 
 
@@ -44,3 +44,22 @@ def case_status_or_notes_changed(sender, instance: Case, **kwargs) -> None:
 
     # Fire the notification via the service helper
     notify_client_case_updated(instance, changed)
+
+@receiver(post_save, sender=CaseNote)
+def case_status_note_changed(sender, instance: CaseNote, created: bool, **kwargs) -> None:
+    """
+    Whenever a status_note (CaseNote) is created or updated,
+    send a 'notes' change notification to the client.
+    """
+    case = instance.case
+
+    # If for some reason there's no client_code, nothing to notify
+    if not case.client_code:
+        return
+
+    # Optionally bump last_update on the Case
+    case.last_update = timezone.now()
+    case.save(update_fields=["last_update"])
+
+    # Re-use the existing 'notes' semantics in notify_client_case_updated
+    notify_client_case_updated(case, changed_fields=["notes"])
